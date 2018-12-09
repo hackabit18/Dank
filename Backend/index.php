7a77d4a1838c07b1ce2956a7688bddfd5fa11ea0
Chaitanya Tyagi <chay2199@gmail.com>
2018-12-09 12:53:05 +0530

    Homepage for the site

---
+++ Backend/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en" >
+    <head>
+    <meta charset="UTF-8">
+    <title>Game To Debug</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
+    <link rel="stylesheet" href="style21.css">
+    </head>
+
+    <body>
+        <div class="container demo">
+        <div class="content">
+            <div id="large-header" class="large-header">
+                <canvas id="demo-canvas"></canvas>
+                <h1 class="main-title">Game<span class="thin"> To </span>Debug</h1>
+                <a href="indextransition.php" class="main-button">Go To Editor</a>
+                <p class="description">Game To Debug is a C++ visual debugger that allows you to track the changes in your code at every step.</p>
+                </div>
+            </div>
+        </div>
+        <script src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/499416/TweenLite.min.js'></script>
+        <script src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/499416/EasePack.min.js'></script>
+        <script src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/499416/demo.js'></script>
+    </body>
+</html>
+
